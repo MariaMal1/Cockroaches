2c5807fc84de97ea993edb574735d481d0664744
LefterisSakakos <lefterissak04@gmail.com>
2025-01-18 19:08:26 +0200

    .
    Έκανα μια μικρή προσθήκη στις ερωτήσεις που κάνουμε στον χρήστη

--- Main.java
+++ Main.java
@@ -5,13 +5,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in).useLocale(java.util.Locale.US);
 
         // Λήψη εισόδων από τον χρήστη
-        System.out.println("Enter the area of the field (in square meters): ");
+        System.out.println("Enter the area of the field (in square meters)(1 hectare = 1000 square meters ): ");
         double area = scanner.nextDouble(); // Ενημερώθηκε σε double
 
         System.out.println("Enter the labor cost per hour: ");
         double laborCost = scanner.nextDouble();
 
-        System.out.println("Enter the available water for irrigation (in liters )(Average required water amount per hectare: 950,000 liters): ");
+        System.out.println("Enter the available water for irrigation (in liters )(Average required water amount per hectare: 950000 liters): ");
         double availableWater = scanner.nextDouble();
 
         // Δημιουργία δεδομένων και εκκίνηση της μηχανής βελτιστοποίησης
